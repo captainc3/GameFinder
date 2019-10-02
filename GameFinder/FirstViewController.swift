310efa51e7057796fbcdd9267d2cd3ac5bfe9419
Steven Corrales <scorrales3@gatech.edu>
2019-10-02 14:43:41 -0400

    Initial Commit

---
+++ GameFinder/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GameFinder
+//
+//  Created by Steven Corrales on 10/2/19.
+//  Copyright © 2019 Steven Corrales. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GameFinder/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GameFinder
+//
+//  Created by Steven Corrales on 10/2/19.
+//  Copyright © 2019 Steven Corrales. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
