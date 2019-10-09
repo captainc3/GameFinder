10fa82052c8da64278b7961c38b98020f2c661fd
Steven Corrales <scorrales3@gatech.edu>
2019-10-09 14:15:53 -0400

    messed with table view more

--- GameFinder/ThirdViewController.swift
+++ GameFinder/ThirdViewController.swift
@@ -8,25 +8,72 @@
 
 import UIKit
 
+struct cellData {
+    var opened = Bool()
+    var title = String()
+    var sectionData = [String]()
+}
+
 class ViewController: UITableViewController {
+    
+    var tableViewData = [cellData]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
         view.backgroundColor = UIColor.mainBlue()
         tabBarItem.title = "View Events"
+        
+        tableViewData = [cellData(opened: false, title: "Title1", sectionData: ["cell1", "cell2", "cell3"]),
+        cellData(opened: false, title: "Title1", sectionData: ["cell1", "cell2", "cell3"]),
+        cellData(opened: false, title: "Title1", sectionData: ["cell1", "cell2", "cell3"])]
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //disposes of any resources that can be recreated
     }
-    */
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return (tableViewData.count + 1)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableViewData[section].opened == true {
+            return tableViewData[section].sectionData.count
+        } else {
+            return 1
+        }
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {return UITableViewCell()}
+            cell.textLabel?.text = tableViewData[indexPath.section].title
+            return cell
+        } else {
+            //use different cell identifier if needed
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {return UITableViewCell()}
+            cell.textLabel?.text = tableViewData[indexPath.section].sectionData[indexPath.row - 1]
+            return cell
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            if tableViewData[indexPath.section].opened == true {
+                tableViewData[indexPath.section].opened == false
+                let sections = IndexSet.init(integer: indexPath.section)
+                tableView.reloadSections(sections, with: .none) //can mess with this later
+            } else {
+                tableViewData[indexPath.section].opened == true
+                let sections = IndexSet.init(integer: indexPath.section)
+                tableView.reloadSections(sections, with: .none) //can mess with this later
+            }
+        }
+    }
 
 }
