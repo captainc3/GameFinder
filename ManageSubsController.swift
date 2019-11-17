1b019b0c46353f8308f24ee7a0a0fbb066e60472
Warren Waleed <warrenwaleed8@gmail.com>
2019-11-17 17:21:23 -0500

    made manage subs button on profile page

---
+++ ManageSubsController.swift
@@ -0,0 +1,9 @@
+//
+//  ManageSubsController.swift
+//  GameFinder
+//
+//  Created by Warren Waleed on 11/17/19.
+//  Copyright © 2019 Steven Corrales. All rights reserved.
+//
+
+import Foundation
