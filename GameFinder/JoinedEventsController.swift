58ca4fc5b765cec8f9698a9a855f780d1573c8a1
Warren Waleed <warrenwaleed8@gmail.com>
2019-11-18 14:12:58 -0500

    Made joined events page

---
+++ GameFinder/JoinedEventsController.swift
@@ -0,0 +1,14 @@
+//
+//  JoinedEventsController.swift
+//  GameFinder
+//
+//  Created by Warren Waleed on 11/18/19.
+//  Copyright © 2019 Steven Corrales. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class JoinedEventsController: UIViewController, UITextFieldDelegate {
+    
+}
